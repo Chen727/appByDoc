bf1fa911eb51b1bc822c86322b2a02c92cac272c
Chen727 <526525205@qq.com>
2017-07-13 14:14:08 +0800

    增加__str__方法

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("appByDoc.settings")#"DJANGO_SETTINGS_MODULE",原来在括号里的环境变量
+                                        #作用是终端中输入命令是不用再输入python+manage.py ...
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
