736b606e12d4079fbe9ffc91edf0981d81190875
Chen727 <526525205@qq.com>
2017-07-13 12:04:22 +0800

    增加__str__方法

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "appByDoc.settings")
+    os.environ.setdefault("appByDoc.settings")#"DJANGO_SETTINGS_MODULE",原来在括号里的环境变量
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
--- polls/models.py
+++ polls/models.py
@@ -5,6 +5,8 @@ from  django.db import models
 class Question(models.Model):
     question_text=models.CharField('问题最大字数',max_length=200)
     pub_date=models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
 class Choice(models.Model):#每个模型类似Choice，有多个变量类似question，
                             # 每个变量代表数据库中的一个字段
     question=models.ForeignKey(Question,on_delete=models.CASCADE)
@@ -15,3 +17,7 @@ class Choice(models.Model):#每个模型类似Choice，有多个变量类似ques
                     #修改你的模型（在models.py文件中）。
                     #运行python manage.py makemigrations ，为这些修改创建迁移文件
                     #运行python manage.py migrate ，将这些改变更新到数据库中。
+    def __str__(self):
+        return self.choice_text#给你的模型添加__str__()方法很重要，
+                                # 不仅会使你自己在使用交互式命令行时看得更加方便,
+                                # 而且会在Django自动生成的管理界面中使用对象的这种表示
