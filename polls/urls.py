90b08e7df6651b65389405a5a3f441039f171ef2
Chen727 <526525205@qq.com>
2017-07-13 18:20:03 +0800

    增加__str__方法

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
 from polls import views
+app_name='polls'
 urlpatterns = [
-    url(r'^$',views.index,name='index'),
-    #app的urls里使用如上写法固定polls的名字，然后在Project下的urls里使用include，
-    #使用include()可以使URL不限制与地址例如$^，只要include里写对 地址写什么都可以
-
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
